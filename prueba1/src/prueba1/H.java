1449ddb30a2caa956811264d7d7361730e679b8d
xavier rosillo <xrosillo@ieslavereda.es>
2021-01-26 13:30:07 +0100

    H

---
+++ prueba1/src/prueba1/H.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prueba1;
+
+/**
+ * @author xrosi
+ *
+ */
+public class H {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
